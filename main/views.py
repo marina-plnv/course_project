d1664eb351f5b45538420e20d3105de102c8f69d
Maryna <mpaluyanava@gmail.com>
2021-11-14 01:25:56 +0300

    header in layout.html added

--- main/views.py
+++ main/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
-    return  render(request, 'main/index.html')
+    movies = Movie.objects.all()
+    context={
+        "movies": movies,
+    }
+    return  render(request, 'main/index.html', context)
+
+def details(request, id):
+    movie=Movie.objects.get(id=id)
+    context={
+        'movie': movie
+    }
+    return render(request, 'main/details.html', context)
