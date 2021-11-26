ca03bc96912f9bdf58c2cfc60d24eb2ac1ef8e9a
Maryna <mpaluyanava@gmail.com>
2021-11-26 23:01:35 +0300

    site_id changed

--- recommendation/settings.py
+++ recommendation/settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-SITE_ID = 1
+SITE_ID = 2
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
